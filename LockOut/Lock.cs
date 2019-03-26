9e984dc72d9ecd15283e790680fc9d5e29071460
evan999 <emeshberge@gmail.com>
2019-03-25 22:57:17 -0400

    Initialize project in local environment.

---
+++ LockOut/Lock.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LockOut
+{
+    public class Lock
+    {
+        public bool incorrectPasscodeAttempts(string passcode, string[] attempts)
+        {
+            int maxAttempts = 10;
+            bool lockOut = false;
+            int consecAttempts = 1;
+
+            for (int numAttempts = 1; numAttempts < attempts.Length; numAttempts++)
+            {
+                if (consecAttempts == 10)
+                {
+                    lockOut = true;
+                    Console.WriteLine("Account has been locked out. Max attempts reached." );
+                    break;
+                }
+
+                if (passcode == attempts[numAttempts])
+                { 
+                    consecAttempts = 1;
+                }
+                else
+                {
+                    consecAttempts++;
+                }
+
+
+            }
+
+            return lockOut;
+        }
+
+    }
+}
--- LockOut/Program.cs
+++ LockOut/Program.cs
@@ -10,6 +10,13 @@ namespace LockOut
     {
         static void Main(string[] args)
         {
+            string[] attempts = { "1111", "4444", "9999", "3333", "8888", "2222", "7777", "0000", "6666", "7285", "5555", "1111" };
+            Lock lock1 = new Lock();
+            var result = lock1.incorrectPasscodeAttempts("1111", attempts);
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+
         }
     }
 }
